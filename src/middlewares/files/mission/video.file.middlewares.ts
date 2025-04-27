e873eaa7fe6f40d4aa1bff608b040dd0fe06a71b
Amit Yadav <faizabadamit23@gmail.com>
2025-04-27 12:05:57 +0530

    add file route

--- src/middlewares/files/mission/video.file.middlewares.ts
+++ src/middlewares/files/mission/video.file.middlewares.ts
@@ -1,23 +1,20 @@
 import multer from "multer";
 import path from "path";
 
-// Define the upload directory
 const uploadDirectory = path.resolve(__dirname, "../../../../assets/thumbnail");
 
-// Multer Storage Configuration
 const storage = multer.diskStorage({
     destination: function (_req, _file, cb) {
-        cb(null, uploadDirectory); // Store videos in this folder
+        cb(null, uploadDirectory);
     },
     filename: function (_req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)); // Unique filename
+        cb(null, Date.now() + path.extname(file.originalname));
     },
 });
 
-// Multer Upload Middleware
 const uploadVideo = multer({
     storage: storage,
-    limits: { fileSize: 100 * 1024 * 1024 }, // 100MB Limit
+    limits: { fileSize: 100 * 1024 * 1024 },
     fileFilter: function (_req, file, cb) {
         if (file.mimetype.startsWith("video/")) {
             cb(null, true);
