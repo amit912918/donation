d000efc89e477e495b2d8227905972590a586917
Amit Yadav <faizabadamit23@gmail.com>
2025-06-04 09:07:22 +0530

    change in job

--- src/routes/home/home.routes.ts
+++ src/routes/home/home.routes.ts
@@ -1,4 +1,4 @@
-import { createBanner, deleteBanner, getAllBanners, updateBanner, uploadBannerImages } from "@/controllers/home/home.controllers";
+import { createBanner, deleteBanner, getAllBanners, getContactOption, updateBanner, updateContactOption, updateHomeData, uploadBannerImages } from "@/controllers/home/home.controllers";
 import bannerImagesMiddleware from "@/middlewares/files/home/image.home.files";
 import { Router } from "express";
 
@@ -9,5 +9,10 @@ appHomeRouterV1.post('/upload-banner-images', bannerImagesMiddleware, uploadBann
 appHomeRouterV1.get("/get-all-banner", getAllBanners);
 appHomeRouterV1.put("/update-banner/:id", updateBanner);
 appHomeRouterV1.delete("/delete-banner/:id", deleteBanner);
+
+// description
+appHomeRouterV1.post("/update-home-data", updateHomeData);
+appHomeRouterV1.get('/get-contact-option', getContactOption);
+appHomeRouterV1.put('/update-contact-option', updateContactOption);
 
 export default appHomeRouterV1;
