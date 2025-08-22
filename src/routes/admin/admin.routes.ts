52edf0a77acda1e40b6ef34ffb8010df4c4351f8
Amit Yadav <amit.yadav@ens.enterprises>
2025-08-22 15:20:08 +0530

    some change

--- src/routes/admin/admin.routes.ts
+++ src/routes/admin/admin.routes.ts
@@ -1,4 +1,4 @@
-import { activeMissionByAdmin, getMentorList, makeRemoveBicholiya, makeRemoveMentor } from "@/controllers/admin/admin.controllers";
+import { activeJobByAdmin, activeMissionByAdmin, getMentorList, makeRemoveBicholiya, makeRemoveMentor } from "@/controllers/admin/admin.controllers";
 import { Router } from "express";
 
 const appAdminRouterV1 = Router();
@@ -7,5 +7,6 @@ appAdminRouterV1.get('/get-mentor-list', getMentorList);
 appAdminRouterV1.put('/make-remove-mentor/:userId', makeRemoveMentor);
 appAdminRouterV1.put('/make-remove-bicholiya/:userId', makeRemoveBicholiya);
 appAdminRouterV1.put('/update-mission-status-by-admin/:missionId', activeMissionByAdmin);
+appAdminRouterV1.put('/update-job-status-by-admin/:jobId', activeJobByAdmin);
 
 export default appAdminRouterV1;
